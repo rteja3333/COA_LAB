f3bea4efe9fbcd059994cbbfa23b3d6c6aae4958
rteja3333 <119887820+rteja3333@users.noreply.github.com>
2024-06-25 00:15:05 +0530

    Add files via upload

---
+++ practise2.s
@@ -0,0 +1,16 @@
+##Write a SPIM program to compute the sum of individual digits of a
+#number with maximum 10 digits.
+#• Input: 12345
+#• Sum: 15
+
+prompt:
+    "enter the number:"
+sum:
+    "the sum of digits is:"
+newline:
+    "\n"
+
+
+.text
+.globl main
+main:
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    cout<<"string is "<<s<<endl;
+    // string c=s.substr(0,1);
+    // cout<<c<<endl;
+
+    getline(cin,s);
+    cout<<s;
+    cout<<"done";
+    getline(cin,s);
+    cout<<s;
+    return 0;
+}
